a00774c3495189f6b636ec8d5f75fc947c7ba5aa
Camilo Albert <pepico.valencia@gmail.com>
2019-09-26 19:28:27 +0200

    Añadimos archivo AdiosMundo.java

---
+++ Adios_Mundo.java
@@ -0,0 +1,12 @@
+package Adios_Mundo;
+
+public class Adios_Mundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println( "adios Mundo");
+	}
+
+}
+
