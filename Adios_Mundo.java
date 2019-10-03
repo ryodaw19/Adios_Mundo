07ffc8c78d2cf64f48b835058247b12d1172adb2
Camilo Albert <pepico.valencia@gmail.com>
2019-10-03 18:28:25 +0200

    experimento con colores

--- Adios_Mundo.java
+++ Adios_Mundo.java
@@ -3,11 +3,13 @@ package Adios_Mundo;
 public class Adios_Mundo {
 
 	public static void main(String[] args) {
+
 		String amarillo = "\033[33m";
 		String verde = "\033[32m";
+		String reset = "\033[0m";
 
-		System.out.println(amarillo+"adios Mundo");
-		System.out.println(verde+"Volvere");
+			System.out.println(amarillo+"adios Mundo" + reset);
+			System.out.println(verde+"Volvere" + reset);
 
 	}
 
